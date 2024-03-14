343873bb10f705b21ba7535e05e3feb9c3539342
jolsten <jolsten@gmail.com>
2024-03-13 21:59:55 -0400

    renamed mean motion to "n" instead of "M"

--- src/tle_reader/main.py
+++ src/tle_reader/main.py
@@ -1,6 +1,6 @@
 import datetime
 from dataclasses import dataclass
-from typing import Sequence, Tuple
+from typing import Tuple
 
 from tle_reader.typing import Classification
 from tle_reader.utils import (
@@ -21,12 +21,12 @@ def parse_tle(line1: str, line2: str) -> Tuple[str]:
     classification = line1[7]
     intl_des = line1[9:17]
     epoch = epoch_to_datetime(line1[18:32])
-    Md = float(line1[33:43])
-    Mdd = scinot_to_float(line1[44:52])
+    nd = float(line1[33:43])
+    ndd = scinot_to_float(line1[44:52])
     Bstar = scinot_to_float(line1[53:61])
     elset_type = int(line1[62])
     element_number = int(line1[64:68])
-    checksum1 = int(line1[68])
+    # checksum1 = int(line1[68])
 
     catalog2 = alpha5_to_number(line2[2:7])
     i = float(line2[8:16])
@@ -34,9 +34,9 @@ def parse_tle(line1: str, line2: str) -> Tuple[str]:
     e = implied_decimal_to_float(line2[26:33])
     p = float(line2[34:42])
     mu = float(line2[43:51])
-    M = float(line2[52:63])
+    n = float(line2[52:63])
     revnum = int(line2[63:68])
-    checksum2 = line1[68]
+    # checksum2 = line2[68]
 
     if catalog1 != catalog2:
         msg = (
@@ -49,9 +49,9 @@ def parse_tle(line1: str, line2: str) -> Tuple[str]:
         "catalog": catalog1,
         "intl_des": intl_des,
         "epoch": epoch,
-        "M": M,
-        "Md": Md,
-        "Mdd": Mdd,
+        "n": n,
+        "nd": nd,
+        "ndd": ndd,
         "bstar": Bstar,
         "type": elset_type,
         "number": element_number,
@@ -90,11 +90,11 @@ class TLE:
     """Argument of Perigee (deg)"""
     mu: float
     """Mean Anomaly (deg)"""
-    M: float
+    n: float
     """Mean Motion (rev/day)"""
-    Md: float
+    nd: float
     """Mean Motion 1st Derivative (rev/day^2)"""
-    Mdd: float
+    ndd: float
     """Mean Motion 2nd Derivative (rev/day^3)"""
     rev: int
     """Revolution Number at Epoch"""
@@ -104,14 +104,6 @@ class TLE:
         """Satellite Catalog Number (Alpha-5 Notation)"""
         return number_to_alpha5(self.catalog)
 
-    @property
-    def line1(self) -> str:
-        """TLE Line 1"""
-
-    @property
-    def line2(self) -> str:
-        """TLE Line 2"""
-
     @classmethod
     def from_lines(cls, line1: str, line2: str) -> "TLE":
         return cls(**parse_tle(line1, line2))
@@ -119,16 +111,16 @@ class TLE:
     def to_lines(self) -> Tuple[str, str]:
         alpha5 = number_to_alpha5(self.catalog)
         epoch = datetime_to_epoch(self.epoch)
-        Md = f"{self.Md: .8f}"
-        Md = Md[0] + Md[2:]
-        Mdd = float_to_scinot(self.Mdd)
+        nd = f"{self.nd: .8f}"
+        nd = nd[0] + nd[2:]
+        ndd = float_to_scinot(self.ndd)
         bstar = float_to_scinot(self.bstar)
 
-        line1 = f"1 {alpha5:5}{self.classification} {self.intl_des} {epoch:14} {Md} {Mdd:8} {bstar:8} {self.type} {self.number:4}"
+        line1 = f"1 {alpha5:5}{self.classification} {self.intl_des} {epoch:14} {nd} {ndd:8} {bstar:8} {self.type} {self.number:4}"
         line1 = line1 + str(compute_checksum(line1))
 
         e = float_to_implied_decimal(self.e)
-        line2 = f"2 {alpha5:5} {self.i:8.4f} {self.r:8.4f} {e:7} {self.p:8.4f} {self.mu:8.4f} {self.M:11.8f}{self.rev:5}"
+        line2 = f"2 {alpha5:5} {self.i:8.4f} {self.r:8.4f} {e:7} {self.p:8.4f} {self.mu:8.4f} {self.n:11.8f}{self.rev:5}"
         line2 = line2 + str(compute_checksum(line2))
 
         return line1, line2
